a5b8d6032b669532a81f82b3bfe2e8dc2409c7f5
bai <baiyshdykanov6@gmail.com>
2022-10-06 00:17:06 +0300

    fixed/bags

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, {useEffect} from 'react';
 import './App.css'
 import Header from "./components/Header/Header";
+import {useTelegram} from "./hooks/useTelegram";
+import { Route, Routes} from "react-router-dom";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 const App  = () => {
 
     const {onToggleButton, tg} = useTelegram();
@@ -9,8 +13,12 @@ const App  = () => {
     },[])
 
     return (
-        <div>
+        <div className="App">
             <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form/>}/>
+            </Routes>
         </div>
     );
 };
